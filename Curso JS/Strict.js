716cc34fd904218f1c693a8be80bb5f6acb82a6b
EdmundoSanchezM <edmundoelpro1@gmail.com>
2021-02-15 01:00:21 -0600

    Uso de strict
    Uso de "use strict" en js

---
+++ Curso JS/Strict.js
@@ -0,0 +1,12 @@
+"use strict"; //Para buenas practicas y no regalar jsjjsjs Primer linea o en funcion
+//x = 10; --No esta definidida
+let x = 10; //--Esta definido 0w0
+console.log(x);
+
+miFuncion();
+
+function miFuncion() {
+  //"use strict"
+  let y = 10;
+  console.log(y);
+}
